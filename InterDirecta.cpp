34193fcba4b99a2001cb0aeb1360038d685fbd93
jhosephu <jehuanacunep@unjbg.edu.pe>
2024-09-13 21:22:17 -0500

    actualizar a binario 1 y 2

--- InterDirecta.cpp
+++ InterDirecta.cpp
@@ -5,6 +5,8 @@ using namespace std;
 void interder(int x[],int n);
 void interizq(int x[], int n);
 void intercen(int x[], int n);
+void interbin(int x[], int n);
+void interbin2(int x[], int n);
 
 int main(){
 	int x[100], n;
@@ -13,7 +15,7 @@ int main(){
 	for(int i=0; i<n; i++){
 		cin>> x[i];
 	}
-	intercen(x, n);
+	interbin(x, n);
 	return 0;
 }
 
@@ -48,6 +50,7 @@ void interizq(int x[], int n){
 		cout<< x[i];
 	}
 }
+
 void intercen(int x[], int n){
 	int aux, cen=1, i=0;
 	while(i<n && cen==1){
@@ -61,6 +64,56 @@ void intercen(int x[], int n){
 			}
 		}
 		i++;
+	}
+	for(int i=0; i<n; i++){
+		cout<< x[i];
+	}
+}
+
+void interbin(int x[], int n){
+	int aux;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n-i; j++){
+			if(x[j]>x[j+1]){
+				aux=x[j];
+				x[j]=x[j+1];
+				x[j+1]=aux;
+			}
+		}
+		for(int j=n-i; j>0; j--){
+			if(x[j]<x[j-1]){
+				aux=x[j];
+				x[j]=x[j-1];
+				x[j-1]=aux;
+			}
+		}	
+	}
+	for(int i=0; i<n; i++){
+		cout<< x[i];
+	}
+}
+
+void interbin2(int x[], int n){
+	int izq=1, der=n, k=n, aux;
+	while(izq<=der){
+		for(int i=der; i>=izq; i--){
+			if(x[i-1]>x[i]){
+				aux= x[i-1];
+				x[i-1]=x[i];
+				x[i]=aux;
+				k=i;
+			}
+		}
+		izq=k+1;
+		for(int i=izq; i<=der; i++){
+			if(x[i-1]>x[i]){
+				aux= x[i-1];
+				x[i-1]=x[i];
+				x[i]=aux;
+				k=i;
+			}
+		}
+		der=k-1;
 	}
 	for(int i=0; i<n; i++){
 		cout<< x[i];
